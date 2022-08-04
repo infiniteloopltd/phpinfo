05a04ca8655ec542df1ce1aa69e5839ec8cf02c6
Fiach Reid <fiach@globaldataconsortium.com>
2022-08-04 18:44:47 +0200

    move to /tmp

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 putenv("DOTNET_CLI_HOME=/tmp");
-$output = shell_exec('dotnet run --project "/home/ec2-user/HelloWorld" 2>&1');
+$output = shell_exec('dotnet run --project "/tmp/HelloWorld" 2>&1');
 echo "$output";
 ?>
