388bdce10f79dd2280ccb26538f322e156feb2b5
Fiach Reid <fiach@globaldataconsortium.com>
2022-08-04 18:39:40 +0200

    again

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-$output = shell_exec(' dotnet run --project "/home/ec2-user/HelloWorld"');
+$output = shell_exec('dotnet run --project "/home/ec2-user/HelloWorld" 2>&1');
 echo "$output";
 ?>
